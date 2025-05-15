97401d17f9000dc1ea92b12fc92f9e8cdbdb8caf
Ritesh Gupta <152794543+RiteshGupta19@users.noreply.github.com>
2025-05-15 21:09:26 +0530

    Update server.js

--- user-service/server.js
+++ user-service/server.js
@@ -6,13 +6,26 @@ const { errorHandler } = require('./middlewares/errorMiddleware');
 const cors = require('cors');
 
 const app = express();
+const allowedOrigins = [
+  'http://taskmanager19.s3-website.ap-south-1.amazonaws.com',
+  'https://d395d44loc2fy0.cloudfront.net',
+  'http://localhost:3000' // optional, useful for local development
+];
+
 const corsOptions = {
-  origin: 'http://3.110.84.68',  
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: 'GET,POST,PUT,DELETE',
   credentials: true,
 };
 
 app.use(cors(corsOptions));
+
 
 connectDB();
 
